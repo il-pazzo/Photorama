27a943355e7280f4ac37385c909779d912e30ef8
Glenn Cole <gcole@mac.com>
2020-06-10 01:01:19 -0700

    Add image caching (using a tweaked ImageStore lifted from LootLogger)

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -16,6 +16,8 @@ enum PhotoError: Error {
 
 class PhotoStore {
     
+    let imageStore = ImageStore()
+    
     private let session: URLSession = {
         
         let config = URLSessionConfiguration.default
@@ -47,6 +49,14 @@ class PhotoStore {
     
     func fetchImage( for photo: Photo,
                      completion: @escaping (Result<UIImage, Error>) -> Void ) {
+        
+        let photoKey = photo.photoID
+        if let image = imageStore.image( forKey: photoKey ) {
+            OperationQueue.main.addOperation {
+                completion( .success( image ))
+            }
+            return
+        }
     
         guard let photoURL = photo.remoteURL else {
             completion( .failure( PhotoError.missingImageURL ))
@@ -58,6 +68,11 @@ class PhotoStore {
         let task = session.dataTask( with: request ) { (data, response, error) in
             
             let result = self.processImageRequest( data: data, error: error )
+            
+            if case let .success( image ) = result {
+                self.imageStore.setImage( image, forKey: photoKey )
+            }
+            
             OperationQueue.main.addOperation {
                 completion( result )
             }
